228bd9ab5c76e8aae6cf19a994a27b1dce45f70f
Adham Salama <adhamsalama@zohomail.com>
2021-11-06 18:55:43 +0200

    Update auth service

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -27,9 +27,19 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     }
+}, {
+    toJSON: {
+        transform(doc, ret) {
+            ret.id = ret.__id;
+            delete ret._id;
+            delete ret.password;
+            delete ret.__v;
+        }
+    }
+
 });
 
-userSchema.pre('save', async function(done) {
+userSchema.pre('save', async function (done) {
     if (this.isModified('password')) {
         const hashed = await Password.toHash(this.get('password'));
         this.set('password', hashed);
--- auth/src/routes/signing.ts
+++ auth/src/routes/signing.ts
@@ -1,9 +1,47 @@
 import express from 'express';
+import { body } from 'express-validator';
+import { Request, Response } from "express";
+import { validateRequest } from '../middlewares/validate-request';
+import { BadRequestError } from '../errors/bad-request-error';
+import { User } from '../models/user';
+import { Password } from '../utils/password';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-    res.send('Hi there!');
-});
+router.post(
+    "/api/users/signin",
+    [
+        body("email").isEmail().withMessage("Email must be valid"),
+        body("password").trim().notEmpty().withMessage("You must supply a password"),
+    ],
+    validateRequest,
+    async (req: Request, res: Response) => {
+        const { email, password }: {email : string, password: string} = req.body;
+
+        const existingUser = await User.findOne({ email });
+        if (!existingUser) {
+            throw new BadRequestError('Invalid credentials');
+        }
+
+        const passwordMatch = await Password.compare(existingUser.password, password);
+        if (!passwordMatch) {
+            throw new BadRequestError('Invalid credentials');
+        }
+        // Generate JWT
+        const userJwt = jwt.sign({
+            id: existingUser.id,
+            email: existingUser.email
+            }, process.env.JWT_KEY!
+        );
+
+        // Store it on session object
+        req.session = {
+            jwt: userJwt
+        };
+        
+        res.status(200).send(existingUser);
+    }
+);
 
 export { router as signinRouter };
