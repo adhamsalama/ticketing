db7be46adbbda88160d7e2bc4bf2352e3c9f8d1a
Adham Salama <adhamsalama@zohomail.com>
2021-11-11 12:40:05 +0200

    Add testing files

---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,59 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('returns a 201 on successful signup', async () => {
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(201);
+});
+
+it('returns a 400 with an invalid email', async () => {
+    await request(app)
+    .post('/api/users/signup')
+    .send({
+        email: 'test',
+        password: 'password'
+    })
+    .expect(400);
+});
+
+it('returns a 400 with an invalid password', async () => {
+    await request(app)
+    .post('/api/users/signup')
+    .send({
+        email: 'test@test.com',
+        password: '1'
+    })
+    .expect(400);
+});
+
+it('returns a 400 with missing email and password', async () => {
+    await request(app)
+    .post('/api/users/signup')
+    .send({})
+    .expect(400);
+});
+
+it('disallows duplicate emails', async () => {
+    await request(app)
+    .post('/api/users/signup')
+    .send({
+        email: 'test@test.com',
+        password: 'password'
+    })
+    .expect(201);
+
+    await request(app)
+    .post('/api/users/signup')
+    .send({
+        email: 'test@test.com',
+        password: 'password'
+    })
+    .expect(400);
+
+    
+});
