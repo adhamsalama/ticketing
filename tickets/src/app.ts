822ecce9877ef88f57ce36847d66657ef9114a20
Adham Salama <adhamsalama@zohomail.com>
2021-11-19 02:48:03 +0200

    Use updateTicketRouter

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,6 +4,7 @@ import cookieSession from 'cookie-session';
 import { createTicketRouter } from './routes/new';
 import { showTicketRouter } from './routes/show';
 import { indexTicketRouter } from './routes';
+import { updateTicketRouter } from './routes/update';
 import { errorHandler, NotFoundError, currentUser } from '@kubertickets/common';
 
 const app = express();
@@ -22,6 +23,7 @@ app.use(currentUser);
 app.use(createTicketRouter);
 app.use(showTicketRouter);
 app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all('*', async (req, res) => {
     throw new NotFoundError();
