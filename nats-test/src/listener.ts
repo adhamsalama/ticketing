b8b415447759ff1665fa2582d8e4f5c8d54b5a9c
Adham Salama <adhamsalama@zohomail.com>
2021-12-06 20:55:39 +0200

    Delete Listener abstract class

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,6 @@
 import nats, { Message, Stan } from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
+import { TicketCreatedListener } from './events/ticket-created-listener';
 
 console.clear();
 
@@ -14,67 +15,9 @@ stan.on('connect', () => {
         console.log('NATS connection closed!');
         process.exit();
     });
+    
     new TicketCreatedListener(stan).listen();
-
 });
 
 process.on('SIGINT', () => stan.close());
 process.on('SIGTERM', () => stan.close());
-
-abstract class Listener {
-
-    private client: Stan;
-    abstract subject: string;
-    abstract queueGroupName: string;
-    protected ackWait = 5 * 1000;
-    abstract onMessage(data: any, msg?: Message): void;
-    
-    constructor(client: Stan) {
-        this.client = client;
-    }
-
-    subscriptionsOptions() {
-        return this.client
-            .subscriptionOptions()
-            .setDeliverAllAvailable()
-            .setManualAckMode(true)
-            .setAckWait(this.ackWait)
-            .setDurableName(this.queueGroupName)
-    }
-
-    listen() {
-        const subscription = this.client.subscribe(
-            this.subject,
-            this.queueGroupName,
-            this.subscriptionsOptions()
-        );
-
-        subscription.on('message', (msg: Message) => {
-            console.log(
-                `Message received: ${this.subject} / ${this.queueGroupName}`
-            );
-            
-            const parsedData = this.parseMessage(msg);
-            this.onMessage(parsedData, msg);
-        });
-
-    }
-
-    parseMessage(msg: Message) {
-        const data = msg.getData();
-        return typeof data === 'string'
-        ? JSON.parse(data)
-        : JSON.parse(data.toString('utf-8'));
-    }
-}
-
-class TicketCreatedListener extends Listener {
-    subject = 'ticket:created';
-    queueGroupName = 'payments-service';
-
-    onMessage(data: any, msg: Message) {
-        console.log('Event data', data);
-
-        msg.ack();
-    }
-}
