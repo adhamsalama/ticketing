45819af519059e55624edddfe50257f90f95f981
Adham Salama <adhamsalama@zohomail.com>
2021-12-16 00:46:08 +0200

    Add TicketUpdatedListener

---
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -0,0 +1,20 @@
+import { Listener, NotFoundError, Subjects, TicketUpdatedEvent } from '@kubertickets/common';
+import { Message } from 'node-nats-streaming';
+import { Ticket } from '../../models/ticket';
+import { queueGroupName } from './queue-grou-name';
+
+export class TicketUpdatedClass extends Listener<TicketUpdatedEvent> {
+    readonly subject = Subjects.TicketUpdated;
+    queueGroupName = queueGroupName;
+
+    async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
+        const ticket = await Ticket.findById(data.id);
+        if (!ticket) throw new NotFoundError();
+
+        const { title, price } = data;
+        ticket.set({ title, price });
+        await ticket.save();
+        
+        msg.ack();
+    }
+}
