a5050c128eeb57ca722654135cc849820c77b4c1
Adham Salama <adhamsalama@zohomail.com>
2021-12-16 00:40:55 +0200

    Add TicketCreatedListener

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -1,9 +1,16 @@
 import { Listener, Subjects, TicketCreatedEvent } from '@kubertickets/common';
 import { Message } from 'node-nats-streaming';
 import { Ticket } from '../../models/ticket';
+import { queueGroupName } from './queue-grou-name';
+
 export class TicketCreatedClass extends Listener<TicketCreatedEvent> {
     readonly subject = Subjects.TicketCreated;
-    queueGroupName = 'orders-service';
+    queueGroupName = queueGroupName;
 
-    onMessage(data: TicketCreatedEvent['data'], msg?: Message) {}
+    async onMessage(data: TicketCreatedEvent['data'], msg: Message) {
+        const { id, title, price } = data;
+        const ticket = Ticket.build({ id, title, price} );
+        await ticket.save();
+        msg.ack();
+    }
 }
