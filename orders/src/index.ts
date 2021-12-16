62af43c1dd5f546cdadd73ec64b656944dea8861
Adham Salama <adhamsalama@zohomail.com>
2021-12-16 12:03:22 +0200

    Add NATS setup and listeners

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -1,5 +1,8 @@
 import mongoose from 'mongoose';
 import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
+import { TicketCreatedListener } from './events/listeners/ticket-created-listener';
+import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener';
 
 const start = async () => {
     if (!process.env.JWT_KEY) {
@@ -8,12 +11,36 @@ const start = async () => {
     if (!process.env.MONGO_URI) {
         throw new Error('MONGO_URI must be defined');
     }
+    if (!process.env.NATS_URL) {
+        throw new Error('NATS_URL must be defined');
+    }
+
+    if (!process.env.NATS_CLIENT_ID) {
+        throw new Error('MONGO_URI must be defined');
+    }
+
+    if (!process.env.NATS_CLUSTER_ID) {
+        throw new Error('MONGO_URI must be defined');
+    }
+
     try {
+        await natsWrapper.connect(process.env.NATS_CLUSTER_ID, process.env.NATS_CLIENT_ID, process.env.NATS_URL);
+        console.log('Index connected to NATS');
+        natsWrapper.client.on('close', () => {
+            console.log('NATS connection closed!');
+            process.exit();
+        });
+        process.on('SIGINT', () => natsWrapper.client.close());
+        process.on('SIGTERM', () => natsWrapper.client.close());
+
+        new TicketCreatedListener(natsWrapper.client).listen();
+        new TicketUpdatedListener(natsWrapper.client).listen();
+        
         await mongoose.connect(process.env.MONGO_URI);
         console.log('Connected to MongoDB')
     }
-    catch (e) {
-        console.log('DB connection error', e);
+    catch (err) {
+        console.log('Connection error', err);
     }
     app.listen(3000, () => {
         console.log('Running at port 3000!!!');
