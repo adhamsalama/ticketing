aaa6b9967b8452f941f38ae7aced853e6618ef6a
Adham Salama <adhamsalama@zohomail.com>
2021-12-19 18:45:49 +0200

    Add parenthesis to invoke the listen method

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -36,7 +36,7 @@ const start = async () => {
 
         new TicketCreatedListener(natsWrapper.client).listen();
         new TicketUpdatedListener(natsWrapper.client).listen();
-        new ExpirationCompleteListener(natsWrapper.client).listen;
+        new ExpirationCompleteListener(natsWrapper.client).listen();
                 
         await mongoose.connect(process.env.MONGO_URI);
         console.log('Connected to MongoDB')
