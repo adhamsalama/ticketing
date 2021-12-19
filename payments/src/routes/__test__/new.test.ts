aaf35ed89f09efa2a9030a83bfa62a6a7c6c1785
Adham Salama <adhamsalama@zohomail.com>
2021-12-19 20:38:17 +0200

    Add test for making a payment

---
+++ payments/src/routes/__test__/new.test.ts
@@ -0,0 +1,59 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Order } from '../../models/order';
+import mongoose from 'mongoose';
+import { signin } from './utils/signing';
+import { OrderStatus } from '@kubertickets/common';
+
+it('returns a 404 when purchasing an order that does not exist', async () => {
+    await request(app)
+        .post('/api/payments')
+        .set('Cookie', signin())
+        .send({
+            token: 'some token',
+            orderId: new mongoose.Types.ObjectId().toHexString() 
+        })
+        .expect(404);
+});
+
+it('returns a 401 when purchasing an order that does not belong to the user ', async () => {
+    const order = Order.build({
+        id: new mongoose.Types.ObjectId().toHexString(),
+        userId: new mongoose.Types.ObjectId().toHexString(),
+        version: 0,
+        status: OrderStatus.Created,
+        price: 30
+    });
+    await order.save();
+
+    await request(app)
+        .post('/api/payments')
+        .set('Cookie', signin())
+        .send({
+            token: 'some token',
+            orderId: order.id 
+        })
+        .expect(401);
+});
+
+it('returns a 400 when purchasing a cancelled event', async () => {
+    const userId = new mongoose.Types.ObjectId().toHexString();
+
+    const order = Order.build({
+        id: new mongoose.Types.ObjectId().toHexString(),
+        userId: userId,
+        version: 0,
+        status: OrderStatus.Cancelled,
+        price: 30
+    });
+    await order.save();
+
+    await request(app)
+        .post('/api/payments')
+        .set('Cookie', signin(userId))
+        .send({
+            token: 'some token',
+            orderId: order.id 
+        })
+    .expect(400);
+});
