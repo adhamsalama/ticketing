1ff8044b9c2e4d2154decf6aba36a6add58a91da
Adham Salama <adhamsalama@zohomail.com>
2021-12-19 16:12:46 +0200

    Add test for OrderCreatedListener

---
+++ payments/src/events/listeners/__test__/order-created-listener.test.ts
@@ -0,0 +1,57 @@
+import { OrderCreatedListener } from '../order-created-listener';
+import { natsWrapper } from '../../../nats-wrapper';
+import { Order } from '../../../models/order';
+import mongoose from 'mongoose';
+import { OrderCreatedEvent, OrderStatus } from '@kubertickets/common';
+
+const setup = async () => {
+    const listener = new OrderCreatedListener(natsWrapper.client);
+    
+    // const order = Order.build({
+    //     id: new mongoose.Types.ObjectId().toHexString(),
+    //     version: 0,
+    //     userId: new mongoose.Types.ObjectId().toHexString(),
+    //     price: 50,
+    //     status: OrderStatus.Created
+    // });
+    // await order.save();
+
+    const data: OrderCreatedEvent['data'] = {
+        id: new mongoose.Types.ObjectId().toHexString(),
+        version: 0,
+        userId: new mongoose.Types.ObjectId().toHexString(),
+        status: OrderStatus.Created,
+        expiresAt: 'does not matter',
+        ticket: {
+            id: 'test',
+            price: 50
+        }
+    };
+
+    // @ts-ignore
+    const msg: Message = {
+        ack: jest.fn()
+    };
+
+    return { listener, data, msg };
+}
+
+it('replicates order info', async () => {
+    const { listener, data, msg } = await setup();
+
+    await listener.onMessage(data, msg);
+
+    const order = await Order.findById(data.id);
+    expect(order).toBeDefined();
+    expect(order!._id.toHexString()).toEqual(data.id);
+    expect(order!.price).toEqual(data.ticket.price);
+    expect(order!.version).toEqual(data.version);
+});
+
+it('acks the message', async () => {
+    const { listener, data, msg } = await setup();
+
+    await listener.onMessage(data, msg);
+
+    expect(msg.ack).toHaveBeenCalled();
+});
