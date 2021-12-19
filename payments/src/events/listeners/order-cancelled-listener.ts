add50f2e086ac9193a082d42a741469fedb01c34
Adham Salama <adhamsalama@zohomail.com>
2021-12-19 17:06:02 +0200

    Implement OrderCancelledListener

---
+++ payments/src/events/listeners/order-cancelled-listener.ts
@@ -0,0 +1,24 @@
+import { Listener, NotFoundError, OrderCancelledEvent, OrderStatus, Subjects } from "@kubertickets/common";
+import { Message } from "node-nats-streaming";
+import { queueGroupName } from './queueGroupName';
+import { Order } from '../../models/order';
+
+export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
+    readonly subject = Subjects.OrderCancelled;
+    queueGroupName = queueGroupName;
+    async onMessage(data: OrderCancelledEvent['data'], msg: Message) {
+        const order = await Order.findOne({
+            _id: data.id,
+            version: data.version-1
+        });
+        if (!order) {
+            msg.ack();
+            throw new NotFoundError();
+        }
+
+        order.set({ status: OrderStatus.Cancelled });
+        await order.save();
+        
+        msg.ack();
+    }
+}
