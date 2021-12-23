56e5cc11169fd51e664d22c1a0f969ff8f22959f
Adham Salama <adhamsalama@zohomail.com>
2021-12-24 01:11:11 +0200

    Show StripeCheckout component

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,8 +1,18 @@
 import { useEffect, useState } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import useRequest from '../../hooks/use-request';
 
-const OrderShow = ({ order }) => {
-    const [timeLeft, setTimeLeft] = useState('');
+const OrderShow = ({ order, currentUser }) => {
+    const [timeLeft, setTimeLeft] = useState(0);
 
+    const { doRequest, errors } = useRequest({
+        url: '/api/payments',
+        method: 'post',
+        body: {
+            orderId: order.id,
+        },
+        onSuccess: (payment) => console.log(payment)
+    });
     useEffect(() => {
         const findTimeLeft = () => {
             const msLeft = new Date(order.expiresAt) - new Date();
@@ -15,7 +25,21 @@ const OrderShow = ({ order }) => {
             clearInterval(timerId);
         };
     }, []);
-    return <div>Time left to pay: {timeLeft} seconds.</div>
+
+    if(timeLeft < 0) {
+        return <div>Order Expired</div>
+    }
+
+    return <div>
+        Time left to pay: {timeLeft} seconds.
+        <StripeCheckout 
+        token={({ id }) => doRequest({ token: id })}
+        stripeKey='pk_test_51K8UVGABm7D5syb0EIihan0dwyTlYdSZTxmqIpOhzOCNGZyOfkeMj228VAd5L6Sr3cUw2c5RqWXKpUVuJgYfGzqj00dFpr1XbI'
+        amount={order.ticket.price * 100}
+        email={currentUser.email}
+    />    
+    {errors}
+    </div>
 };
 
 OrderShow.getInitialProps = async (context, client) => {
