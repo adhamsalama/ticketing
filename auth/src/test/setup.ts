8cd9da40f975f28d338d676f4e01745cc21357d4
Adham Salama <adhamsalama@zohomail.com>
2021-12-24 17:03:15 +0200

    Update MongoMemoryServer setup

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,32 +1,31 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
 import { app } from '../app';
 
-let mongo: any;
-beforeAll(async () => {
-    // Use a a MongoDB coker image instead of MongoMemoryServer
-    // because MongoMemoryServer checks mongodb.com for Ubuntu 21.10 which doesn't exist yet
-    mongo = await MongoMemoryServer.create();
-    const mongoUri = mongo.getUri();
-    await mongoose.connect(mongoUri);
-    // await mongoose.connect('mongodb://localhost:27017');
-    process.env.JWT_KEY = 'asdf';
-});
 
+let mongo: any;
+//hook function means this will run before all of
+//our tests start excecuting.
+beforeAll(async () => {
+  process.env.JWT_KEY = 'BrB';
+
+  mongo = new MongoMemoryServer();
+  await mongo.start();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri);
+});
+//reset all the data between each test runs.
 beforeEach(async () => {
-    const collections = await mongoose.connection.db.collections();
-    for (let collection of collections) {
-        await collection.deleteMany({});
-    }
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
 });
 
 afterAll(async () => {
-    await mongo.stop();
-    
-    // Delete collections after finishing the tests because I'm using a persistent MongoDB instance
-    const collections = await mongoose.connection.db.collections();
-    for (let collection of collections) {
-        await collection.deleteMany({});
-    }
-    await mongoose.connection.close();
-})
+  await mongo.stop();
+});
+
