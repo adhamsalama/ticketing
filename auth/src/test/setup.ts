e7a57e12d192f6203f9c0e0fab654e9412c3145d
Adham Salama <adhamsalama@zohomail.com>
2021-12-24 16:46:07 +0200

    Use MongoMemoryServer

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -6,10 +6,10 @@ let mongo: any;
 beforeAll(async () => {
     // Use a a MongoDB coker image instead of MongoMemoryServer
     // because MongoMemoryServer checks mongodb.com for Ubuntu 21.10 which doesn't exist yet
-    // mongo = await MongoMemoryServer.create({ binary: {version: '21.04'}});
-    // const mongoUri = mongo.getUri();
-    // await mongoose.connect(mongoUri);
-    await mongoose.connect('mongodb://localhost:27017');
+    mongo = await MongoMemoryServer.create({ binary: {version: '21.04'}});
+    const mongoUri = mongo.getUri();
+    await mongoose.connect(mongoUri);
+    // await mongoose.connect('mongodb://localhost:27017');
     process.env.JWT_KEY = 'asdf';
 });
 
@@ -21,7 +21,7 @@ beforeEach(async () => {
 });
 
 afterAll(async () => {
-    //await mongo.stop();
+    await mongo.stop();
     
     // Delete collections after finishing the tests because I'm using a persistent MongoDB instance
     const collections = await mongoose.connection.db.collections();
